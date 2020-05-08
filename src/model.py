cc5765214f0add831bce40e23ff3c399c44e8344
Dmitry Kostyanoy <dmitry.kostyanoy@gmail.com>
2020-05-08 09:49:40 +0200

    - Added testing per class - Added unet model without skip connections

--- src/model.py
+++ src/model.py
@@ -1,6 +1,52 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+class UNet(nn.module):
+
+    def down_block(self, in_dim, out_dim):
+        return nn.Sequential(
+            nn.Conv2d(in_dim, out_dim, 3),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_dim, 3, 3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+    def up_block(self, in_dim, out_dim):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_dim, out_dim, 2),
+            nn.Conv2d(out_dim, out_dim, 3),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_dim, out_dim, 3),
+            nn.ReLU(inplace=True)
+        )
+
+    def __init__(self):
+        super(UNet, self).__init__()
+        self.down1 = self.down_block(3, 32)
+        self.down2 = self.down_block(32, 64)
+        self.down3 = self.down_block(64, 128)
+        self.down4 = self.down_block(128, 256)
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(256, 512, 3),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3),
+            nn.ReLU(inplace=True)
+        )
+        self.up1 = self.up_block(512, 256)
+        self.up2 = self.up_block(256, 128)
+        self.up3 = self.up_block(128, 64)
+        self.up3 = self.up_block(64, 32)
+
+        self.output = nn.Sequential(
+            nn.Conv2d(32, 1, 1),
+            nn.Sigmoid(),
+        )
+
+
+    # def forward(self, x):
+
+
+
 
 class Net(nn.Module):
 
--- testing.py
+++ testing.py
@@ -7,6 +7,9 @@ import numpy as np
 from src.model import Net
 import torch.optim as optim
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
 
 def imshow(img):
     img = img / 2 + 0.5
@@ -26,10 +29,11 @@ classes = ('plane', 'car', 'bird', 'cat',
 
 dataiter = iter(testloader)
 images, labels = dataiter.next()
-imshow(torchvision.utils.make_grid(images))
+# imshow(torchvision.utils.make_grid(images))
 print('Ground truth: ', ' '.join(classes[labels[i]] for i in range(4)))
 
 model = Net()
+model.to(device)
 model.load_state_dict(torch.load('/Users/stranger/PycharmProjects/unet-pytorch/models/test2.pt'))
 # model.eval()
 outputs = model(images)
@@ -39,13 +43,24 @@ print('Predicted: ', ' '.join(classes[predicted[i]] for i in range(4)))
 
 correct = 0
 total = 0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
 with torch.no_grad():
     for data in testloader:
-        images, labels = data
+        images, labels = data[0].to(device), data[1].to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels)
+        for j in range(4):
+            label = labels[j]
+            class_correct[label] += int(c[j].item())
+            class_total[label] += 1
         total += labels.size(0)
         correct += (labels == predicted).sum().item()
 print('Accuracy: ', 100 * correct / total)
+for i in range(10):
+    print('accuracy for class', classes[i], class_correct[i]/class_total[i] *100)
+
+
 
 
