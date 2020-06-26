47e92a35b3d5b22eeb8ca5b3d97d111f31bcad52
alponomar <alen.ponomareva@gmail.com>
2020-06-26 11:08:27 +0200

    - added sweep config - made get_accuracy function from testing_unet.py

--- src/testing_unet.py
+++ src/testing_unet.py
@@ -1,68 +1,37 @@
 import torch
-import torchvision
-import torchvision.transforms as standard_transforms
-from src.model import UNet
-import numpy as np
-import matplotlib.pyplot as plt
+
+def get_accuracy(testloader, net, device):
+    right_pixels = 0
+    count = 0
+    with torch.no_grad():
+        for i, data in enumerate(testloader):
+            test_images, test_labels = data
+            test_labels[test_labels != 0] = 1.0
+            test_images, test_labels = test_images.to(device), test_labels.to(device)
+            outputs = net(test_images)
+            outputs[outputs > 0.5] = 1
+            outputs[outputs < 0.5] = 0
+
+            # output = outputs.to('cpu')[0].data.numpy()
+            # label = test_labels.to('cpu')[0].data.numpy()
+            # image = test_images.to('cpu')[0].data.numpy()
+            # imshow(image)
+            # imshow(output)
+            # imshow(label)
+
+            # labels = torch.squeeze(labels, dim=1)
+            right_pixels_current = (outputs == test_labels).sum().item() / (256 * 256) * 100
+            right_pixels += right_pixels_current
+            count += test_labels.size(0)
+            # print(right_pixels_current)
+        return right_pixels / count
 
 
-def imshow(npimg):
-    npimg = np.transpose(npimg, (1, 2, 0))
-    if npimg.shape[2] == 1:
-        npimg = npimg[:, :, 0]
-        zers = np.zeros((256, 256, 3))
-        zers[:, :, 0] = npimg
-        zers[:, :, 1] = npimg
-        zers[:, :, 2] = npimg
-        npimg = zers
-    plt.imshow(npimg)
-    plt.show()
 
 
-USE_CUDA = torch.cuda.is_available()
-device = torch.device("cuda:0" if USE_CUDA else "cpu")
-
-input_transform = standard_transforms.Compose([
-    standard_transforms.CenterCrop(256),
-    standard_transforms.ToTensor()
-])
-target_transform = standard_transforms.Compose([
-    standard_transforms.CenterCrop(256),
-    standard_transforms.ToTensor()
-])
-
-testset = torchvision.datasets.VOCSegmentation(root='/home/elena/PycharmProjects/unet-pytorch/voc_data', year='2008',
-                                               image_set='val',
-                                               download=True, transform=input_transform,
-                                               target_transform=target_transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=8)
-
-net = UNet()
-net.to(device)
+#
+# net = UNet()
+# net.to(device)
 # net.load_state_dict(
 #     torch.load('/home/elena/PycharmProjects/unet-pytorch/src/wandb/run-20200624_081139-3rg4aznj/unet_wb.pt'))
-
-right_pixels = 0
-count = 0
-with torch.no_grad():
-    for i, data in enumerate(testloader):
-        images, labels = data
-        labels[labels != 0] = 1.0
-        images, labels = images.to(device), labels.to(device)
-        outputs = net(images)
-        outputs[outputs > 0.5] = 1
-        outputs[outputs < 0.5] = 0
-
-        # output = outputs.to('cpu')[0].data.numpy()
-        # label = labels.to('cpu')[0].data.numpy()
-        # image = images.to('cpu')[0].data.numpy()
-        # imshow(image)
-        # imshow(output)
-        # imshow(label)
-
-        # labels = torch.squeeze(labels, dim=1)
-        right_pixels_current = (outputs == labels).sum().item() / (256 * 256) * 100
-        right_pixels += right_pixels_current
-        count += labels.size(0)
-        # print(right_pixels_current)
-print('Accuracy %d procent' % (right_pixels / count))
+#
--- src/training_unet.py
+++ src/training_unet.py
@@ -5,10 +5,19 @@ import torchvision.transforms as standard_transforms
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-from src.model import UNet
+from model import UNet
+from testing_unet import get_accuracy
 import torch.optim as optim
 import wandb
+import argparse
+
+hyperparameter_defaults = dict(
+    learning_rate=0.001,
+    epochs=10
+)
+
 wandb.init(project="test")
+
 
 def imshow(img):
     npimg = img.numpy()
@@ -28,58 +37,73 @@ USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda:0" if USE_CUDA else "cpu")
 
 
-input_transform = standard_transforms.Compose([
-    standard_transforms.CenterCrop(256),
-    standard_transforms.ToTensor()
-])
-target_transform = standard_transforms.Compose([
-    standard_transforms.CenterCrop(256),
-    standard_transforms.ToTensor()
-])
-trainset = torchvision.datasets.VOCSegmentation(root='/home/elena/PycharmProjects/unet-pytorch/voc_data', year='2008',
-                                                image_set='train',
-                                                download=True, transform=input_transform,
-                                                target_transform=target_transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+def train():
+    input_transform = standard_transforms.Compose([
+        standard_transforms.CenterCrop(256),
+        standard_transforms.ToTensor()
+    ])
+    target_transform = standard_transforms.Compose([
+        standard_transforms.CenterCrop(256),
+        standard_transforms.ToTensor()
+    ])
+    trainset = torchvision.datasets.VOCSegmentation(root='/home/elena/PycharmProjects/unet-pytorch/voc_data', year='2008',
+                                                    image_set='train',
+                                                    download=True, transform=input_transform,
+                                                    target_transform=target_transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=8)
 
-print('training data loading finished')
-print(len(trainloader))
+    print('training data loading finished')
 
-# testset = torchvision.datasets.VOCSegmentation(root='/Users/stranger/PycharmProjects/unet-pytorch/voc_data', year='2012', image_set='trainval',
-#                                      download=True, transform=input_transform, target_transform=target_transform)
-# testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
+    testset = torchvision.datasets.VOCSegmentation(root='/home/elena/PycharmProjects/unet-pytorch/voc_data', year='2008',
+                                                   image_set='val',
+                                                   download=True, transform=input_transform,
+                                                   target_transform=target_transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=8)
 
-# print(len(testloader))
+    print('validation data loading finished')
 
-print('validation data loading finished')
 
-net = UNet()
-criterion = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.0001)
-net.to(device)
 
-wandb.watch(net)
+    wandb.init(config=hyperparameter_defaults)
+    config = wandb.config
 
-for epoch in range(100):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        images, labels = data
-        labels = labels.data
-        labels[labels!=0] = 1.0
-        images, labels = images.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = net(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        # wandb.log({"training loss:", loss.item()})
-        wandb.log({"Batch Loss": loss.item()})
+    net = UNet()
+    criterion = nn.BCEWithLogitsLoss()
+    if config.optimizer == 'adam':
+        optimizer = optim.Adam(net.parameters(), lr=config.learning_rate)
+    else:
+        optimizer = optim.SGD(net.parameters(), lr=config.learning_rate)
+    net.to(device)
 
-    print('%d loss %.3f' % (epoch + 1, running_loss / len(trainloader)))
-    wandb.log({"Total Loss": running_loss / len(trainloader), "Epoch": epoch+1})
+    wandb.watch(net)
 
-print('Finished training')
+    for epoch in range(config.epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            images, labels = data
+            labels = labels.data
+            labels[labels != 0] = 1.0
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            # wandb.log({"training loss:", loss.item()})
+            wandb.log({"Batch Loss": loss.item()})
+        if epoch % 4 == 0:
+            accuracy = get_accuracy(testloader, net, device)
+            wandb.log({"Accuracy": accuracy})
+            print("Accuracy:", accuracy)
 
-# path_to_model = '/home/elena/PycharmProjects/unet-pytorch/models/unet2.pt'
-torch.save(net.state_dict(), os.path.join(wandb.run.dir, 'unet_wb.pt'))
+        print('%d loss %.3f' % (epoch + 1, running_loss / len(trainloader)))
+        wandb.log({"total_loss": running_loss / len(trainloader), "Epoch": epoch + 1})
+
+    print('Finished training')
+
+    # path_to_model = '/home/elena/PycharmProjects/unet-pytorch/models/unet2.pt'
+    torch.save(net.state_dict(), os.path.join(wandb.run.dir, 'unet_wb.pt'))
+
+if __name__ == "__main__":
+    train()
