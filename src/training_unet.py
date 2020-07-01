9505369377d92d960da2c358a119bcc16c5c70a9
alponomar <alen.ponomareva@gmail.com>
2020-07-01 10:50:41 +0200

    - experimenting with artifacts - experimenting adding images to wandb

--- src/training_unet.py
+++ src/training_unet.py
@@ -13,11 +13,11 @@ import argparse
 
 hyperparameter_defaults = dict(
     learning_rate=0.001,
-    epochs=10
+    epochs=5,
+    optimizer='adam'
 )
 
-wandb.init(project="test")
-
+print(hyperparameter_defaults)
 
 def imshow(img):
     npimg = img.numpy()
@@ -63,11 +63,19 @@ def train():
     print('validation data loading finished')
 
 
-
-    wandb.init(config=hyperparameter_defaults)
+    run = wandb.init(project="test",
+                     config=hyperparameter_defaults,
+                     job_type='train')
     config = wandb.config
 
+    artifact = wandb.Artifact('voc_train_2008_sets', type='dataset')
+    artifact.add_dir('/home/elena/PycharmProjects/unet-pytorch/voc_data/VOCdevkit/VOC2008/ImageSets/Segmentation')
+    run.log_artifact(artifact)
+
     net = UNet()
+
+    # artifact_model = wandb.Artifact('unet_model', type='model')
+
     criterion = nn.BCEWithLogitsLoss()
     if config.optimizer == 'adam':
         optimizer = optim.Adam(net.parameters(), lr=config.learning_rate)
@@ -79,13 +87,29 @@ def train():
 
     for epoch in range(config.epochs):
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(trainloader):
             images, labels = data
             labels = labels.data
             labels[labels != 0] = 1.0
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = net(images)
+            if i == 0:
+                for j in images:
+                    mask_imgs = []
+                    mask_img = wandb.Image(images[j], masks={
+                        "predictions": {
+                            "mask_data": outputs[j]
+                        },
+                        "groud_truth": {
+                            "mask_data": labels[j]
+                        }
+                    })
+                    mask_imgs.append(mask_img)
+
+                wandb.log({"masked_examples": [mask_imgs(j) for j in mask_imgs]})
+                # wandb.log({"labels": [wandb.Image(i) for i in labels]})
+                # wandb.log({"predictions": [wandb.Image(i) for i in outputs]})
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
